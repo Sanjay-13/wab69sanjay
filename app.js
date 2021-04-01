4395d69eb7f6b232a6ea448c900a206cf3fd891d
Sanjay-13 <69335575+Sanjay-13@users.noreply.github.com>
2021-03-31 22:48:41 -0500

    added images in slot

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var universityRouter = require('./routes/university');
 var usersRouter = require('./routes/users');
+var starsRouter = require('./routes/stars');
+var slotRouter = require('./routes/slot');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/university', universityRouter);
 app.use('/users', usersRouter);
+app.use('/stars', starsRouter);
+app.use('/slot', slotRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
