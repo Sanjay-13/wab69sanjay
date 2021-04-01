7b60336ef4153a09526a0a4cc85adb25fcf23232
Sanjay-13 <69335575+Sanjay-13@users.noreply.github.com>
2021-03-31 21:54:27 -0500

    added display results

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var universityRouter = require('./routes/university');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/university', universityRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
